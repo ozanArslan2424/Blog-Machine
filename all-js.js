f71e58ceed68e4538a184201ee9fd4fd48cb8d44
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2023-08-25 18:16:29 +0300

    Tutorial added + small fixes

--- all-js.js
+++ all-js.js
@@ -1,4 +1,3 @@
-
 //Upload Img
 var uploaded = true;
 const cells = document.getElementsByClassName("grid-cell");
@@ -111,7 +110,7 @@ wbg.addEventListener("click", () => {
     wbgimg.classList.add("bgplaced");
     wbgimg.setAttribute("id", "WBGID");
     wbgimg.src = whiteBG;
-    
+
     if (cell.querySelector(".bgplaced") !== null) {
       cell.removeChild(document.getElementById("BBGID"));
       cell.appendChild(wbgimg);
@@ -119,7 +118,6 @@ wbg.addEventListener("click", () => {
       cell.appendChild(wbgimg);
     }
   }
-
 });
 
 bbg.addEventListener("click", () => {
@@ -141,13 +139,12 @@ bbg.addEventListener("click", () => {
       cell.appendChild(bbgimg);
     }
   }
-
 });
 
 wbaslik.addEventListener("click", () => {
   wbaslik.classList.add("clicked");
   bbaslik.classList.remove("clicked");
-  
+
   const whiteTitle = "./images/all/SVG/wtitle-prev.svg";
   const wtitleimg = document.createElement("img");
   wtitleimg.classList.add("titleplaced");
@@ -160,7 +157,6 @@ wbaslik.addEventListener("click", () => {
   } else {
     cells[0].appendChild(wtitleimg);
   }
-
 });
 
 bbaslik.addEventListener("click", () => {
@@ -179,7 +175,6 @@ bbaslik.addEventListener("click", () => {
   } else {
     cells[0].appendChild(btitleimg);
   }
-
 });
 
 wt.addEventListener("click", () => {
@@ -191,14 +186,13 @@ wt.addEventListener("click", () => {
   wtpocgimg.classList.add("tpocgplaced");
   wtpocgimg.setAttribute("id", "WTpocgID");
   wtpocgimg.src = whiteTpocg;
-  
+
   if (cells[0].querySelector(".tpocgplaced") !== null) {
     cells[0].removeChild(document.getElementById("BTpocgID"));
     cells[0].appendChild(wtpocgimg);
   } else {
     cells[0].appendChild(wtpocgimg);
   }
-
 });
 
 bt.addEventListener("click", () => {
@@ -217,7 +211,6 @@ bt.addEventListener("click", () => {
   } else {
     cells[0].appendChild(btpocgimg);
   }
-
 });
 
 wl.addEventListener("click", () => {
@@ -229,14 +222,13 @@ wl.addEventListener("click", () => {
   wloncaimg.classList.add("loncaplaced");
   wloncaimg.setAttribute("id", "WLoncaID");
   wloncaimg.src = whiteLonca;
-  
+
   if (cells[0].querySelector(".loncaplaced") !== null) {
     cells[0].removeChild(document.getElementById("BLoncaID"));
     cells[0].appendChild(wloncaimg);
   } else {
     cells[0].appendChild(wloncaimg);
   }
-
 });
 
 bl.addEventListener("click", () => {
@@ -255,14 +247,11 @@ bl.addEventListener("click", () => {
   } else {
     cells[0].appendChild(bloncaimg);
   }
-
 });
 
 //Başlık Kapak
 
-
 function addBaslikText() {
-
   const baslikInput = document.getElementById("baslik-text").value;
   const baslikOutput = document.createElement("div");
   baslikOutput.classList.add("baslik-text", "bytext");
@@ -279,7 +268,6 @@ function addBaslikText() {
 
 //Yazar Kapak
 function addYazarText() {
-  
   const yazarInput = document.getElementById("yazar-text").value;
   const yazarOutput = document.createElement("div");
   yazarOutput.classList.add("yazar-text", "bytext");
@@ -317,7 +305,7 @@ function addMetinText() {
         baslikText.innerText = baslikInput;
         baslikText.setAttribute("contenteditable", "true");
         cells[1].appendChild(baslikText);
-        
+
         const metinText = document.createElement("div");
         metinText.classList.add("metinplaced");
         metinText.innerText = "Yazı buraya gelecek.";
@@ -338,7 +326,7 @@ function addMetinText() {
       }
     }
   }
-// başlık
+  // başlık
 
   // "paste" event
   const metinText = document.getElementsByClassName("metinplaced");
@@ -366,5 +354,56 @@ const slideSection = document.getElementById("phonewarning");
 const slideButton = document.getElementById("slideButton");
 
 slideButton.addEventListener("click", () => {
-      slideSection.style.display = "none";
+  slideSection.style.display = "none";
+});
+
+const tutorialOpenBtn = document.getElementById("tut-show");
+const tutorialCloseBtn = document.getElementById("tut-close");
+const tutorialNextBtn = document.getElementById("tut-next");
+const tutorialSection = document.getElementById("tutorialSection");
+const tutorialWrap = document.getElementById("tutorial-wrap");
+const tutorialText = document.getElementById("tutorialText");
+
+const textContentArray = [
+  "Merhaba, blog makinesine hoşgeldin. Bu baloncuk ile siteyi nasıl kullanacağını öğreneceksin. <br>Baloncuğu kapatmak için sol üstteki çarpıya tıkla. <br>Bir sonraki adım için çarpının yanındaki oka tıkla. <br>",
+  "Öncelikle 'Görsel Seç' yazan tuş ile blog görselini yükle.<br>Yüklediğin görsel ve yapacağın diğer değişiklikleri aşağıda canlı görebilirsin.",
+  "Şimdi siyah/beyaz tuşlardan biriyle yazının arkasına koyulacak rengi seç. <br>Yazının rengi otomatik ayarlanacak.",
+  "Blog görseline uygun olduğunu düşündüğün siyah/beyaz 'Akıl Defterim, TPÖÇG ve Lonca' logolarını seç.",
+  "Blogun başlığını ve yazar ismini yazıp her ikisinin de yanındaki onaylama tuşuna tıkla.<br><br><b>Not: </b>Onay tuşlarına tıklamazsan yazılar yerleştirilmez.<br>Bir hata yaparsan <b>bir sonraki adıma geçmeden</b> yazıyı değiştirip tekrar onay tuşlarına basabilirsin.",
+  "Önceki adımları sorunsuz geçtikten sonra 'Metin gir.' yazılı tuşa tıklayıp blog içeriğini yerleştirmeye başlayabilirsin.<br><br><b>Not: </b>Yazıları aşağıdaki görseller üzerinde düzenleyeceksin.",
+  "Son adım olarak '(Sayfa Sayısı Seç)' yazan menüden tasarımın kaç sayfa olduğunu seçip altındaki 'Yazdır!' tuşu ile bitirebilirsin.<br><br>Kolay gelsin hocam :)",
+];
+let currentTextIndex = 0;
+let isMoved = false;
+
+let currentPositionIndex = 0;
+const positions = [68, 140, 384, 744, 984, 984];
+
+tutorialNextBtn.addEventListener("click", () => {
+  currentTextIndex = (currentTextIndex + 1) % textContentArray.length;
+  tutorialText.style.opacity = 0;
+
+  setTimeout(() => {
+    tutorialText.innerHTML = textContentArray[currentTextIndex];
+    tutorialText.style.opacity = 1;
+  }, 500);
+
+  if (currentPositionIndex < positions.length) {
+    tutorialWrap.style.left = positions[currentPositionIndex] + "px";
+    currentPositionIndex++;
+  } else {
+    tutorialSection.style.display = "none";
+  }
+});
+
+tutorialCloseBtn.addEventListener("click", () => {
+  tutorialSection.style.display = "none";
+  currentPositionIndex = 0;
+  tutorialWrap.style.left = "0";
+});
+
+tutorialOpenBtn.addEventListener("click", () => {
+  tutorialSection.style.display = "block";
+  tutorialWrap.style.left = "60px";
+  tutorialText.innerHTML = textContentArray[0];
 });
